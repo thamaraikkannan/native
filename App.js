224b973f85bbf1743bbed0472d3cdb96dd86b20e
kannan <e.thamaraikannan@gmail.com>
2020-07-31 17:21:57 +0530

    first commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yeah i'm working on it</Text>
       <StatusBar style="auto" />
     </View>
   );
